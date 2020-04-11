ea37a930d95c6cb4c918bb2237af2ae50d453c5e
PatrykFrackowiak <patryk.frackowiak90@gmail.com>
2020-04-11 11:46:32 +0200

    Add generation of title links

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,33 @@
 'use strict'
 
+
+function generateTitleLinks() {
+    const selectors = {
+        article: '.post',
+        title: '.post-title',
+        titleList: '.titles'
+    };
+
+    const generate = function () {
+        const generatedLinks = generateNewLinks();
+        document.querySelector(selectors.titleList).innerHTML = generatedLinks;
+        addEventListenerToTitleLinks();
+    }
+
+    const generateNewLinks = function() {
+        let links = '';
+        for (let article of document.querySelectorAll(selectors.article)) {
+            const articleId = article.getAttribute('id');
+            const articleTitle = article.querySelector(selectors.title).innerHTML;
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+            links += linkHTML;
+        }
+        return links;
+    }
+
+    generate();
+}
+
 const addEventListenerToTitleLinks = function () {
     for (let link of document.querySelectorAll('.titles a')) {
         link.addEventListener('click', titleClickHandler);
@@ -40,3 +68,4 @@ const titleClickHandler = function (event) {
 
     handle(this);
 }
+
