70a57b5a815979e07bb953c80ca0d29aedcbc97d
PatrykFrackowiak <patryk.frackowiak90@gmail.com>
2020-04-19 07:56:32 +0200

    Add eslint stuff

--- js/functions.js
+++ js/functions.js
@@ -1,47 +1,47 @@
-'use strict'
+'use strict';
 
 const addEventListenerToTitleLinks = function () {
-    for (let link of document.querySelectorAll('.titles a')) {
-        link.addEventListener('click', titleClickHandler);
-    }
-}
+  for (let link of document.querySelectorAll('.titles a')) {
+    link.addEventListener('click', titleClickHandler);
+  }
+};
 
 const generateTitleLinks = function () {
-    const SELECTOR = {
-        ARTICLE: '.post',
-        TITLE: '.post-title',
-        TITLE_LIST: '.titles'
-    };
+  const SELECTOR = {
+    ARTICLE: '.post',
+    TITLE: '.post-title',
+    TITLE_LIST: '.titles'
+  };
 
-    let links = '';
-    for (let article of document.querySelectorAll(SELECTOR.ARTICLE)) {
-        const articleId = article.getAttribute('id');
-        const articleTitle = article.querySelector(SELECTOR.TITLE).innerHTML;
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-        links += linkHTML;
-    }
+  let links = '';
+  for (let article of document.querySelectorAll(SELECTOR.ARTICLE)) {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(SELECTOR.TITLE).innerHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    links += linkHTML;
+  }
 
-    document.querySelector(SELECTOR.TITLE_LIST).innerHTML = links;
-    addEventListenerToTitleLinks();
-}
+  document.querySelector(SELECTOR.TITLE_LIST).innerHTML = links;
+  addEventListenerToTitleLinks();
+};
 
 const changeActiveElement = (element, elementSelector) => {
-    for (let activeLink of document.querySelectorAll(elementSelector)) {
-        activeLink.classList.remove('active');
-    }
-    element.classList.add('active');
-}
+  for (let activeLink of document.querySelectorAll(elementSelector)) {
+    activeLink.classList.remove('active');
+  }
+  element.classList.add('active');
+};
 
 const titleClickHandler = function (event) {
-    const SELECTOR = {
-        ARTICLE: 'article.active',
-        TITLELINK: '.titles a.active',
-    };
+  const SELECTOR = {
+    ARTICLE: '.article.active',
+    TITLELINK: '.titles a.active',
+  };
     
-    event.preventDefault();
-    changeActiveElement(this, SELECTOR.TITLELINK);
-    const articleId = this.getAttribute('href');
-    const article = document.querySelector(articleId);
-    changeActiveElement(article, SELECTOR.ARTICLE);
-}
+  event.preventDefault();
+  changeActiveElement(this, SELECTOR.TITLELINK);
+  const articleId = this.getAttribute('href');
+  const article = document.querySelector(articleId);
+  changeActiveElement(article, SELECTOR.ARTICLE);
+};
 
